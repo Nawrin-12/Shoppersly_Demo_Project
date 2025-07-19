d452fa26295acc66e8bd94b3e964d1c4603b9d87
Ishrat Nur Nawrin <ishratnurnawrin5032@gmail.com>
2025-07-19 22:23:02 +0600

    Product Delete

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/product-update', [ProductController::class, 'update']);
+Route::delete('/product-delete', [ProductController::class, 'delete']);
+Route::get('/product-delete', [ProductController::class, 'SoftDeletedData']);
