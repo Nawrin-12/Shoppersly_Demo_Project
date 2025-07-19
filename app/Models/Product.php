f763de2e5340136bc025cb2ac680aac9ee59f2e7
Ishrat Nur Nawrin <ishratnurnawrin5032@gmail.com>
2025-07-19 11:11:08 +0600

    Password Update

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+
+class Product extends Model
+{
+    use HasFactory, Notifiable;
+    protected $table = 'products';
+    protected $fillable = [
+        'name',
+        'description',
+        'category',
+        'price',
+        'url',
+        'status',
+        'user_id',
+
+    ];
+}
---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'description' => $this->faker->paragraph,
+            'category' => $this->faker->word,
+            'price' => $this->faker->randomFloat(2,1,10),
+            'url' => $this->faker->word,
+            'status' => $this->faker->randomElement(['available', 'unavailable']),
+            'user_id' => User::factory()->vendor(),
+        ];
+    }
+}
