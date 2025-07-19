c0c2ca59621686317fd273808c68c6a1fd6bb0d7
Ishrat Nur Nawrin <ishratnurnawrin5032@gmail.com>
2025-07-19 11:10:51 +0600

    Password Update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
@@ -13,6 +14,7 @@ class UserSeeder extends Seeder
         User::create([
             'name' => 'Admin User',
             'email' => 'admin@example.com',
+            'number' => '1234567555',
             'password' => Hash::make('password'),
             'role' => 'admin',
         ]);
@@ -20,6 +22,7 @@ class UserSeeder extends Seeder
         User::create([
             'name' => 'Vendor User',
             'email' => 'vendor@example.com',
+            'number' => '1234437890',
             'password' => Hash::make('password'),
             'role' => 'vendor',
         ]);
@@ -27,8 +30,14 @@ class UserSeeder extends Seeder
         User::create([
             'name' => 'Regular User',
             'email' => 'user@example.com',
+            'number' => '1234567730',
             'password' => Hash::make('password'),
             'role' => 'user',
+        ]);
+
+        $vendor = User::factory()->vendor()->create();
+        Product::factory()->count(1)->create([
+            'user_id' => $vendor->id,
         ]);
     }
 }
