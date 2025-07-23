a2fb506160715a3a49f4e6628ebe511a13a9a7e1
Ishrat Nur Nawrin <ishratnurnawrin5032@gmail.com>
2025-07-23 19:06:33 +0600

    order list

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,7 @@ use App\Models\OrderImage;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
@@ -14,5 +15,15 @@ class Order extends Model
     {
         return $this->hasMany(OrderImage::class);
     }
-    
+
+    public function product(): belongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user(): belongsTo
+    {
+        return $this->belongsTo(User::class, 'customer_user_id');
+    }
+
 }
